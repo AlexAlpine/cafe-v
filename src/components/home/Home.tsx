d0332c85870674dae15d8c873dc58ad6089329c8
AlexAlpine <alex6122223@gmail.com>
2025-06-04 19:11:47 +0200

    add 3 fotos

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './Home.module.css';
+import photo from '../assets/photo.png';
+import photo1 from '../assets/photo1.png';
+import photo2 from '../assets/photo2.png';
 
 export default function Home() {
   return (
@@ -14,6 +17,17 @@ export default function Home() {
             Frisch, schnell und lecker – direkt aus deinem Lieblingsladen mitten in der Stadt.
           </p>
           <button className={styles.button}>Speisekarte ansehen</button>
+        </div>
+
+        {/* 🔼 Новое одиночное изображение */}
+        <div className={styles.photoImage}>
+          <img src={photo} alt="Hauptgericht" />
+        </div>
+
+        {/* 📸 Галерея из двух изображений */}
+        <div className={styles.gallery}>
+          <img src={photo1} alt="Snack 1" />
+          <img src={photo2} alt="Snack 2" />
         </div>
       </div>
     </section>
